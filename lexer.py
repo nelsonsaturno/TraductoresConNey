2d46c2cc7fba7bc7b68b9c77d0084a062537a7d5
nelsonsaturno <nelsonsaturno@gmail.com>
2015-03-07 13:57:36 -0430

    tratando de hacer los flags

--- lexer.py
+++ lexer.py
@@ -158,14 +158,14 @@ def t_error(t):
 
 # Funcion que verifica si se encontro algun error, en este caso se imprime en pantalla
 # En caso contrario se imprimen los tokens encontrados.
-def lexicografico(archivo):
+def lexicografico(archivo,imprimir=None):
 	# Lista en la que se guardan los tokens encontrados en el archivo de texto
 	tokensEncontrados =[]
 
 	try:
 
 		# Abrimos el archivo
-		data = open(sys.argv[1])
+		data = open(archivo)
 
 		# Leemos el archivo
 		data2 = data.read()
@@ -188,39 +188,40 @@ def lexicografico(archivo):
 		print 'ERROR: No se pudo abrir el archivo \"%s\".' % archivo
 		exit()
 
-	# Se imprimen los errores o tokens encontrados
-	if (len(listaErrores) == 0):
-		for a in tokensEncontrados:
-			print "token " + a.type.upper(),
-			if (a.type == "Llave_Cierra")|(a.type == "Llave_Abre")|\
-				 (a.type == "GreaterEqual")|(a.type == "NumElemConj")|\
-				 (a.type == "Par_Cierra")|(a.type == "LessEqual"):
-				print "	value (",
-			else:
-				print "		value (",
-			if (a.type == "ID")|(a.type == "String"):
-				print "\""+a.value+"\"" + " )",
-			else:
-				print str(a.value) + " )",
-			if (a.lineno > 1):
-				print "(Linea " + str(a.lineno) + ", Columna " + \
-							str(find_column(data2,a)) + ")"
-			else:
-				print "(Linea " + str(a.lineno) + ", Columna " + \
-							str(find_column(data2,a)+1) + ")"
-		lexer.lineno =1
-	else:
-		for b in listaErrores:
-			if (b.lineno > 1):
-				print "Error: Se encontró un caracter inesperado " + "'"+\
-							b.value[0]+"'" + " en Linea " + str(b.lineno)+", Columna "+\
-							str(find_column(data2,b)) + "."
-			else:
-				print "Error: Se encontró un caracter inesperado " + "'"+b.value[0]+\
-							"'" + " en Linea " + str(b.lineno)+", Columna "+\
-							str(find_column(data2,b)+1) + "."
+	if imprimir:
+		# Se imprimen los errores o tokens encontrados
+		if (len(listaErrores) == 0):
+			for a in tokensEncontrados:
+				print "token " + a.type.upper(),
+				if (a.type == "Llave_Cierra")|(a.type == "Llave_Abre")|\
+					 (a.type == "GreaterEqual")|(a.type == "NumElemConj")|\
+					 (a.type == "Par_Cierra")|(a.type == "LessEqual"):
+					print "	value (",
+				else:
+					print "		value (",
+				if (a.type == "ID")|(a.type == "String"):
+					print "\""+a.value+"\"" + " )",
+				else:
+					print str(a.value) + " )",
+				if (a.lineno > 1):
+					print "(Linea " + str(a.lineno) + ", Columna " + \
+								str(find_column(data2,a)) + ")"
+				else:
+					print "(Linea " + str(a.lineno) + ", Columna " + \
+								str(find_column(data2,a)+1) + ")"
 			lexer.lineno =1
-		exit()
+		else:
+			for b in listaErrores:
+				if (b.lineno > 1):
+					print "Error: Se encontró un caracter inesperado " + "'"+\
+								b.value[0]+"'" + " en Linea " + str(b.lineno)+", Columna "+\
+								str(find_column(data2,b)) + "."
+				else:
+					print "Error: Se encontró un caracter inesperado " + "'"+b.value[0]+\
+								"'" + " en Linea " + str(b.lineno)+", Columna "+\
+								str(find_column(data2,b)+1) + "."
+				lexer.lineno =1
+			exit()
 
 
 
